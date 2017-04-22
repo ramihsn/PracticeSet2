cdfd7ee18bce134ef689f63848ac5ca688aeebf2
Rami LT <hsn.rami@gmail.com>
2017-04-22 21:08:11 +0300

    Fist part is over     quantity is declared     price is declared

--- app/src/main/java/com/ramilearningandroid/justjava/MainActivity.java
+++ app/src/main/java/com/ramilearningandroid/justjava/MainActivity.java
@@ -30,7 +30,10 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        display(1);
+        int num = 1;
+        int price = 5;
+        display(num);
+        displayPrice(num*price);
     }
 
     /**
@@ -39,7 +42,6 @@ public class MainActivity extends AppCompatActivity {
     private void display(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
-        displayPrice(number);
     }
 
     /**
